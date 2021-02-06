7b7b7374c58ec24b457670c9a0e17062a6b62f1d
Gabriel <gaplogan@gmail.com>
2021-02-06 09:23:06 -0300

    2021.02.06.09.23

---
+++ ValidaErrado.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <locale>
+#include <cmath> // math.h
+
+using namespace std;
+
+int main()
+{
+    // Configurar idioma para português
+    setlocale(LC_ALL, "Portuguese");
+
+    //Declaração de variáveis
+    double num, raiz;
+
+    //{entrada de dados}
+    cout << "Digite um número positivo: ";
+    cin >> num;
+
+    // Validação dos dados (FORMA ERRADA)
+    if (num < 0) //ISTO NÃO ADIANTA. O FLUXO DO PROGRAMA CONTINUARÁ!! 
+    {
+        cout << "Valor Inválido";
+    }
+
+    cout << endl << endl; // Pular 2 linhas
+
+    //{Processamento: Tenta extrair a raiz quadrada do número lido}
+    raiz = sqrt(num); //AQUI SERÁ GERADO UM ERRO SE "Num" FOR NEGATIVO !!!
+
+    cout << "Raiz quadrada de " << num << ": " << raiz << endl;
+
+    return 0;
+}
